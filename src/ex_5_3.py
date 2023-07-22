e7e144fb4bc0977ceb1aeec83d2b922f75db5be5
SalmaSyed143 <136188447+SalmaSyed143@users.noreply.github.com>
2023-07-21 23:22:35 -0500

    Update ex_5_3.py

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -16,4 +16,14 @@ if __name__ == "__main__":
     # Tests will run your command using a system call.
     # To test your program with arguments, run it from the command line
     # (see README.md for more details)
-    pass
+    parsArgument = ArgumentParser(description="This program applies a standard scale transform to the data in infile and writes it to outfile")
+    parsArgument.add_argument("infile", type=argparse.FileType('r'))
+    parsArgument.add_argument("outfile", type=argparse.FileType('w'))
+    parase_args_ = parsArgument.parse_args()
+    os.makedirs(root_dir / "outputs", exist_ok=True)
+    info = np.loadtxt(parase_args_.infile)
+    meanDF = np.mean(info)
+    meanDF_zero = info - meanDF
+    _std = np.std(meanDF_zero)
+    processed = meanDF_zero / _std
+    np.savetxt(parase_args_.outfile, processed,fmt='%.2e')
